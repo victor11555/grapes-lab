7fe333ab4cb0efee3c9639f41379afd797b8dacc
Vladislav <semakin91@gmail.com>
2021-02-04 17:33:38 +0300

    added components

---
+++ front/src/components/common/ProjectCard/ProjectCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Vote from '../Vote/Vote';
+
+function ProjectCard({id, author, projectName, concept, rating, status}) {
+
+	return (
+		<div>
+
+			<Card>
+				<Card.Body>
+					<Card.Title>ProjectName</Card.Title>
+					<Card.Subtitle className="mb-2 text-muted">Author</Card.Subtitle>
+					<Card.Text>
+						ConceptConceptConceptConceptConceptConceptConceptConceptConceptConceptConceptConcept
+					</Card.Text>
+				</Card.Body>
+				<Card.Footer>
+					<small className="text-muted">Status</small>
+					<small className="text-muted">Rating</small>
+					<small className="text-muted">
+						<Vote/>
+					</small>
+				</Card.Footer>
+			</Card>
+
+		</div>
+	);
+}
+
+export default ProjectCard;
---
+++ front/src/components/common/ProjectInfo/ProjectInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ProjectInfo(props) {
+	return (
+		<div>
+
+
+
+		</div>
+	);
+}
+
+export default ProjectInfo;
