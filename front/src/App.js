47cf738263e0a1ef3a9978236ef32fd9ad9a3a80
Fira Salimov <fira9494@mail.ru>
2021-02-04 14:35:55 +0300

    Structure_OF_React_Components_1.0

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className='App'>
-
+      <SignUp/>
     </div>
   );
 }
--- front/src/components/common/SignUpBox/SignUpBox.js
+++ front/src/components/common/SignUpBox/SignUpBox.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import {} from "react-bootstrap";
+import {Form, Button} from "react-bootstrap";
 
-function SignUpBox(props) {
+function SignUpBox() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Form>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control name={'name'} type="text" placeholder="Enter your name" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control name={'email'} type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPhone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control name={'phone'} type="phone" placeholder="Enter your phone number" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCompany">
+          <Form.Label>Company</Form.Label>
+          <Form.Control name={'company'} type="text" placeholder="Enter your company" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Role select</Form.Label>
+          <Form.Control name={'role'} as="select">
+            <option>Admin</option>
+            <option>Stakeholder</option>
+            <option>Support</option>
+            <option>Curator</option>
+            <option>User</option>
+          </Form.Control>
+        </Form.Group>
+        <Button name={'signUpBtn'} variant="primary" type="submit">
+          Sign Up
+        </Button>
+      </Form>
+    </>
   );
 }
 
