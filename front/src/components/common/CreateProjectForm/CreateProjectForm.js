fa95d31231e6c951f138160a886b307a894c273e
Vladislav <semakin91@gmail.com>
2021-02-04 23:02:40 +0300

    fix backend and add createProjectForm component

--- front/src/components/common/CreateProjectForm/CreateProjectForm.js
+++ front/src/components/common/CreateProjectForm/CreateProjectForm.js
@@ -1,102 +1,126 @@
-import {Button, Form} from "react-bootstrap";
-import React, {useState} from "react";
-import {useDispatch} from "react-redux";
-import {userSignupAC} from "../../../store/actions/signup.actions";
+import {Button, Form} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {createProjectAC} from '../../../store/actions/project.actions';
+
 
 function CreateProjectForm(props) {
-    const dispatch = useDispatch();
+	const [select, setSelect] = useState(false);
+	const dispatch = useDispatch();
 
-    const submitHandler = (e) => {
-        e.preventDefault()
-        const {
-            concept: {value: concept},
-            projectName: {value: projectName},
-            description: {value: description},
-            phone: {value: phone},
-            company: {value: company},
-            role: {value: role},
-        } = e.target
-        if (select) {
-            const {secret: {value: secret}} = e.target;
-            dispatch(userSignupAC({name, email, password, phone, company, role, secret}))
-        } else {
-            dispatch(userSignupAC({name, email, password, phone, company, role}))
-        }
-    }
-    const needPrototype = <Form.Group>
-        <Form.Label>Prototype Text</Form.Label>
-        <Form.Control name='prototypeText' type="text" required/>
-    </Form.Group>
-    const [select, setSelect] = useState(false)
-    const viewNeedPrototype = (e) => {
-        if (e.target.value == 'No') {
-            setSelect(false)
-        } else {
-            setSelect(true)
-        }
-    }
-    return (
-        <Form onSubmit={submitHandler}>
-            <Form.Group>
-                <Form.Label>Concept</Form.Label>
-                <Form.Control name='concept' type="text" placeholder="Enter the concept of the project" required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Project name</Form.Label>
-                <Form.Control name='projectName' type="text" required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="textArea" name='description' required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Project result</Form.Label>
-                <Form.Control type="textArea" name='projectResult' required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Technology</Form.Label>
-                <Form.Control type="textArea" name='technology' required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Comparison</Form.Label>
-                <Form.Control type="textArea" name='comparison' required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Basis</Form.Label>
-                <Form.Control type="textArea" name='basis' required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Needs</Form.Label>
-                <Form.Control type="textArea" name='needs' required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>target Client</Form.Label>
-                <Form.Control type="textArea" name='targetClient' required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Acceptable Outcome</Form.Label>
-                <Form.Control type="textArea" name='acceptableOutcome' required/>
-            </Form.Group>
-            <Form.Group controlId="Form.ControlSelect1">
-                <Form.Label>Need prototype</Form.Label>
-                <Form.Control onChange={viewNeedPrototype} name={'needPrototype'} as="select">
-                    <option>No</option>
-                    <option>Yes</option>
-                </Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Additional Needs</Form.Label>
-                <Form.Control name={'additionalNeeds'} as="select">
-                    <option>No</option>
-                    <option>Yes</option>
-                </Form.Control>
-            </Form.Group>
-            {select ? needPrototype : null}
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-    );
+	const submitHandler = (e) => {
+		e.preventDefault();
+		const {
+			concept: {value: concept},
+			projectName: {value: projectName},
+			description: {value: description},
+			projectResult: {value: projectResult},
+			technology: {value: technology},
+			comparison: {value: comparison},
+			basis: {value: basis},
+			needs: {value: needs},
+			targetClient: {value: targetClient},
+			acceptableOutcome: {value: acceptableOutcome},
+			needPrototype: {value: needPrototype},
+			additionalNeeds: {value: additionalNeeds},
+		} = e.target;
+
+		const token = JSON.parse(localStorage.getItem('jwt'));
+
+		dispatch(createProjectAC({
+			token,
+			concept,
+			projectName,
+			description,
+			projectResult,
+			technology,
+			comparison,
+			basis,
+			needs,
+			targetClient,
+			acceptableOutcome,
+			needPrototype,
+			additionalNeeds
+		}));
+	};
+
+	const needPrototype = <Form.Group>
+		<Form.Label>Prototype Text</Form.Label>
+		<Form.Control name='prototypeText' type="text" required/>
+	</Form.Group>;
+
+	const viewNeedPrototype = (e) => {
+		if (e.target.value === 'No') {
+			setSelect(false);
+		} else {
+			setSelect(true);
+		}
+	};
+
+	return (
+		<>
+			<Form onSubmit={ submitHandler }>
+				<Form.Group>
+					<Form.Label>Concept</Form.Label>
+					<Form.Control name='concept' type="text" placeholder="Enter the concept of the project" required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Project name</Form.Label>
+					<Form.Control name='projectName' type="text" required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Description</Form.Label>
+					<Form.Control type="textArea" name='description' required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Project result</Form.Label>
+					<Form.Control type="textArea" name='projectResult' required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Technology</Form.Label>
+					<Form.Control type="textArea" name='technology' required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Comparison</Form.Label>
+					<Form.Control type="textArea" name='comparison' required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Basis</Form.Label>
+					<Form.Control type="textArea" name='basis' required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Needs</Form.Label>
+					<Form.Control type="textArea" name='needs' required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>target Client</Form.Label>
+					<Form.Control type="textArea" name='targetClient' required/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Acceptable Outcome</Form.Label>
+					<Form.Control type="textArea" name='acceptableOutcome' required/>
+				</Form.Group>
+				<Form.Group controlId="Form.ControlSelect1">
+					<Form.Label>Need prototype</Form.Label>
+					<Form.Control onChange={ viewNeedPrototype } name={ 'needPrototype' } as="select">
+						<option>No</option>
+						<option>Yes</option>
+					</Form.Control>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Additional Needs</Form.Label>
+					<Form.Control name={ 'additionalNeeds' } as="select">
+						<option>No</option>
+						<option>Yes</option>
+					</Form.Control>
+				</Form.Group>
+				{ select ? needPrototype : null }
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</>
+	);
 }
 
 export default CreateProjectForm;
--- front/src/store/sagas/createProject.saga.js
+++ front/src/store/sagas/createProject.saga.js
@@ -5,16 +5,20 @@ import { addErrorAC } from '../actions/error.actions';
 import {addProjectAC} from "../actions/user.actions";
 
 async function RequestCreateProject(payload) {
+    console.log('fetch');
     const response = await fetch(CREATE_PROJECT_URL, {
         method: 'POST',
         headers: {'Content-type': 'Application/json'},
         body: JSON.stringify(payload)
     })
+    console.log(response);
     return await response.json()
 }
 
 function* createProjectWorker({payload}) {
+    console.log('worker');
     const response = yield call(RequestCreateProject, payload);
+    console.log(response);
     if (response.success) yield put(addProjectAC(response.project))
     yield put(addErrorAC(response.message));
 }
