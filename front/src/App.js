fdd7e1d50eefede4fb20dd49d6f52225c492983d
Fira Salimov <fira9494@mail.ru>
2021-02-04 18:02:11 +0300

    Navbar_Added

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
+import { Switch, Route } from 'react-router-dom';
+import NavBar from './components/common/Navbar/Navbar';
 
 function App() {
   return (
     <div className='App'>
-      <SignUp/>
-      <Login/>
+      <NavBar/>
+      <Switch>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/signup' component={SignUp}/>
+      </Switch>
     </div>
   );
 }
