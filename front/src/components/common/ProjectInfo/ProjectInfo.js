15db980a117730ddbe79d52a84ec088ec7a8aac0
Vladislav <semakin91@gmail.com>
2021-02-04 18:02:28 +0300

    added components v.1.1

--- front/src/components/common/ProjectInfo/ProjectInfo.js
+++ front/src/components/common/ProjectInfo/ProjectInfo.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import {Container} from 'react-bootstrap';
+import CommentList from '../CommentList/CommentList';
+import RoadMap from '../RoadMap/RoadMap';
 
-function ProjectInfo(props) {
+function ProjectInfo({id, author, projectName, concept, rating, status}) {
 	return (
 		<div>
 
+			<Container>
+				MAP:
+				ProjectName
+				Author
+				Concept
+				Rating
+				Status
 
+				<CommentList/>
+				<RoadMap/>
+
+
+				<Button variant="primary">Связаться с ...</Button>{ ' ' }
+				<Button variant="primary">Импорт в PDF</Button>
+			</Container>
 
 		</div>
 	);
