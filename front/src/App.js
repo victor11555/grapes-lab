6516f18c164c5a7a4c2ba605b36e4b419de27f6f
Fira Salimov <fira9494@mail.ru>
2021-02-04 15:48:20 +0300

    Srcret_Key

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className='App'>
       <SignUp/>
+      <Login/>
     </div>
   );
 }
---
+++ front/src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginBox from '../../components/common/ LoginBox/LoginBox';
+
+function Login(props) {
+  return (
+    <div>
+      <LoginBox/>
+    </div>
+  );
+}
+
+export default Login;
