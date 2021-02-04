8c36f3df3660ed29aa41dea6db4e37c5d03b549c
Sendoff74 <sendoff74@gmail.com>
2021-02-04 12:57:17 +0300

    new store

--- front/src/store/reducers/root.reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { authreducer } from './auth.reducer';
-
-export const rootReducer = combineReducers({
-  user: authreducer,
-})
--- front/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-import {rootReducer} from './reducers/root.reducer';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import { applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)))
