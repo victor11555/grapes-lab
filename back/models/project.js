c14ee1330bb9e435824b90e45413b6152579cfa5
Victor <vzabrovskiu@gmail.com>
2021-02-04 13:36:53 +0300

    models

---
+++ back/models/project.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  status: { type: String, required: true },
+  concept: { type: String, required: true },
+  projectResult: { type: String, required: true },
+  technology: { type: String, required: true },
+  comparison: { type: String, required: true },
+  basis: { type: String, required: true },
+  needs: { type: String, required: true },
+  targetClient: { type: String, required: true },
+  acceptableOutcome: { type: String, required: true },
+  needPrototype: { type: Boolean, required: true, default: false },
+  additionalNeeds: { type: String, required: true },
+  team: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}],
+  moderating: { type: Boolean, required: true, default: false },
+});
+
+module.exports = mongoose.model('Project', projectSchema);
