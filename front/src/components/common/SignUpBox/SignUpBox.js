a741a0a9962be7dd28ea087eb1f63259797f2d19
Fira Salimov <fira9494@mail.ru>
2021-02-04 14:08:53 +0300

    Sign_Up_Box_1.0

---
+++ front/src/components/common/SignUpBox/SignUpBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from "react-bootstrap";
+
+function SignUpBox(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default SignUpBox;
---
+++ front/src/pages/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpBox from '../../components/common/SignUpBox/SignUpBox';
+
+function SignUp(props) {
+  return (
+    <div>
+      <SignUpBox/>
+    </div>
+  );
+}
+
+export default SignUp;
