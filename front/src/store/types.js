f4944b353bf1ee1a244b9abb44c2f0ded2018330
Anna <anya.mezentseva59@yandex.ru>
2021-02-05 10:06:39 +0300

    pdf saga

--- front/src/store/types.js
+++ front/src/store/types.js
@@ -5,3 +5,4 @@ export const GET_PROFILE = 'GET_PROFILE';
 export const ADD_USER = 'ADD_USER';
 export const CREATE_PROJECT = 'CREATE_PROJECT';
 export const ADD_PROJECT = 'ADD_PROJECT';
+export const ADD_PDF = 'ADD_PDF';
