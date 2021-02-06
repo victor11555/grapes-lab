c680a852ae220b11a42a84d54f8930df61523829
Victor <vzabrovskiu@gmail.com>
2021-02-06 05:11:08 +0300

    saga for editproject? editProjectAC

--- front/src/store/reducers/project.reducer.js
+++ front/src/store/reducers/project.reducer.js
@@ -1,4 +1,4 @@
-import { GET_ALL_PROJECTS, SAVE_VOTED_PROJECT } from '../types';
+import { GET_ALL_PROJECTS, SAVE_CHANGES_PROJECT, SAVE_VOTED_PROJECT } from '../types';
 
 export const projectReducer = (state = [], action) => {
   switch (action.type) {
@@ -6,6 +6,9 @@ export const projectReducer = (state = [], action) => {
       return [...action.payload];
     case SAVE_VOTED_PROJECT:
       console.log('saga work');
+      return;
+    case SAVE_CHANGES_PROJECT:
+      console.log('saga work2');
       return;
     default:
       return state;
