bba31612344335d80eaa260d0df604f5c5fb0d5c
Anna <anya.mezentseva59@yandex.ru>
2021-02-06 14:15:21 +0300

    edit saga

--- front/src/store/reducers/user.reducer.js
+++ front/src/store/reducers/user.reducer.js
@@ -1,4 +1,4 @@
-import {ADD_PROJECT, ADD_USER, DELETE_PROJECT_USER_STATE, USER_LOGOUT} from '../types';
+import {ADD_PROJECT, ADD_USER, DELETE_PROJECT_USER_STATE, SAVE_CHANGES_PROJECT, USER_LOGOUT} from '../types';
 
 export const userReducer = (state = {}, action) => {
   switch (action.type) {
@@ -7,6 +7,12 @@ export const userReducer = (state = {}, action) => {
 
     case USER_LOGOUT:
       return state = {};
+
+    case SAVE_CHANGES_PROJECT:
+      const index = state.projects.findIndex(el => el._id == action.payload._id);
+      state.projects[index] = action.payload
+      console.log(state.projects)
+      return {...state}
 
     case ADD_PROJECT:
       return { ...state, projects: [...state.projects, action.payload] };
